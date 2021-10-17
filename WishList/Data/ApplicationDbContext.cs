82bf8e5ee70cbe1f76dcccec5fa553cadc171265
jborowiak <jan.borowiak.jb58@outlook.com>
2021-10-17 19:54:50 +0200

    Fix

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿// Copyright © Titian Software Ltd
+
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+            
+        }  
+    }
+}
