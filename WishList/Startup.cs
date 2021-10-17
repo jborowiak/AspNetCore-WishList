fe7c3fcbef17b8187ccb3b89bf15e07dff80fff9
jborowiak <jan.borowiak.jb58@outlook.com>
2021-10-17 19:36:28 +0200

    Fix

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -31,8 +31,11 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseRouting()
-                .UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
