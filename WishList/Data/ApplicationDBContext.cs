a9b6c0f7cfd53ee4bc2abca92fec0594fe423e5f
jborowiak <jan.borowiak.jb58@outlook.com>
2021-10-17 19:53:24 +0200

    DbContext

---
+++ WishList/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿// Copyright © Titian Software Ltd
+
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDBContext(DbContextOptions options): base(options)
+        {
+            
+        }  
+    }
+}
---
+++ WishList/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿// Copyright © Titian Software Ltd
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Description { get; set; }
+    }
+}
